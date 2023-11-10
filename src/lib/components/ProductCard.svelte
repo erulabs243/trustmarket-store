b22cba1d2094b7721ffcb0efa402fc56947cb068
Glorieux Lukama <ir.antoineglorieux@gmail.com>
2023-11-10 11:05:39 +0200

    fix (ui): align grid cols on mobile

--- src/lib/components/ProductCard.svelte
+++ src/lib/components/ProductCard.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
   import type { ProductType } from "$lib/types/apiType";
   import { displayCurrency } from "$lib/utils/lang";
-  import { goto } from "$app/navigation";
 
   export let product: ProductType;
 
-  const navigateTo = () => goto(`/store/${product.handle}`);
 </script>
 
-  <div class="card bg-base-200 shadow-xl" on:click={navigateTo}>
+  <div class="card bg-base-200 shadow-xl relative text-base">
+    <a href={`/store/${product.handle}`} class="absolute top-0 bottom-0 left-0 right-0"></a>
     <figure class="mx-2 lg:mx-4 mt-2 lg:mt-4 h-44 bg-neutral-300 rounded-xl">
       <img src={product.thumbnail} alt={product.title} class="object-cover" />
     </figure>
     <div class="card-body items-center text-center -mt-4 md:mt-0 max-h-44">
-      <h4 class="card-title font-normal text-base lg:text-lg leading-none lg:leading-normal">{product.title}</h4>
+      <h4 class="card-title font-normal text-sm lg:text-lg leading-none lg:leading-normal">{product.title}</h4>
       {#if product.variants}
         <p class="font-bold">{displayCurrency(product.variants[0].prices[0].amount)}</p>
         <div class="card-actions">
